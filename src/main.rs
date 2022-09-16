527d89a4433cbb88eb882dfc84a65978d62b89c9
João M. Bezerra <marcospb19@hotmail.com>
2022-09-16 15:35:03 -0300

    implement basic websocket connection to BitStamp
    dependencies added:
    - tokio             (async runtime + async std utils)
    - tokio-tungstenite (async websockets library)
    - futures           (traits and utils for async)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use futures::stream::StreamExt;
+use tokio::io::{self, AsyncWriteExt};
+use tokio_tungstenite as websocket;
+
+const BITSTAMP_WEBSOCKET_URL: &str = "wss://ws.bitstamp.net";
+
+#[tokio::main]
+async fn main() {
+    let (mut read_stream, _response) = websocket::connect_async(BITSTAMP_WEBSOCKET_URL)
+        .await
+        .unwrap();
+
+    println!("BitStamp handshake success!!!");
+
+    while let Some(message) = read_stream.next().await {
+        let data = message.unwrap().into_data();
+        io::stdout().write_all(&data).await.unwrap();
+    }
 }
