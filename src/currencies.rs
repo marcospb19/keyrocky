7404aa1ce77ee91b3acda15f6758572be2dd4630
João M. Bezerra <marcospb19@hotmail.com>
2022-09-17 01:06:17 -0300

    create currencies.rs and implement CurrencyPair
    CurrencyPair represents a pair of the two currencies from which the book
    orders will be about, for example, "ETHBTC" and "ADAEUR"

---
+++ src/currencies.rs
@@ -0,0 +1,45 @@
+use std::{fmt, str::FromStr};
+
+use crate::Error;
+
+pub struct CurrencyPair(String);
+
+impl CurrencyPair {
+    pub fn as_str(&self) -> &str {
+        &self.0
+    }
+}
+
+impl fmt::Display for CurrencyPair {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        self.as_str().fmt(formatter)
+    }
+}
+
+impl FromStr for CurrencyPair {
+    type Err = Error;
+
+    fn from_str(text: &str) -> Result<Self, Self::Err> {
+        let is_ascii_string = text.is_ascii();
+        let has_expected_size = text.len() == 6;
+        let is_alphabetic_text = text.chars().all(char::is_alphabetic);
+
+        let is_valid = is_ascii_string && has_expected_size && is_alphabetic_text;
+
+        if !is_valid {
+            return Err(Error::CurrencyPairBadFormat(text.to_owned()));
+        }
+
+        Ok(CurrencyPair(text.to_owned()))
+    }
+}
+
+/// All pairs that are supported by Binance and Bitstamp.
+pub const SUPPORTED_CURRENCY_PAIRS: [&str; 49] = [
+    "AAVEBTC", "ADABTC", "ADAEUR", "ALGOBTC", "APEEUR", "AUDIOBTC", "AVAXEUR", "BCHBTC", "BCHEUR",
+    "BTCEUR", "BTCGBP", "BTCPAX", "BTCUSDC", "BTCUSDT", "CHZEUR", "DOTEUR", "ENJEUR", "ETHBTC",
+    "ETHEUR", "ETHGBP", "ETHPAX", "ETHUSDC", "ETHUSDT", "FTMEUR", "GALAEUR", "GRTEUR", "LINKBTC",
+    "LINKEUR", "LINKGBP", "LTCBTC", "LTCEUR", "LTCGBP", "MATICEUR", "NEAREUR", "OMGBTC", "SHIBEUR",
+    "SOLEUR", "SXPEUR", "UNIBTC", "UNIEUR", "USDCUSDT", "WBTCBTC", "XLMBTC", "XLMEUR", "XRPBTC",
+    "XRPEUR", "XRPGBP", "XRPUSDT", "YFIEUR",
+];
