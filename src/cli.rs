4d3e0958353eeab4c9677986f8d09ad574490eda
João M. Bezerra <marcospb19@hotmail.com>
2022-09-17 01:08:36 -0300

    created cli.rs to argparse CLI options
    added dependencies:
    - clap (CLI argparsing)

---
+++ src/cli.rs
@@ -0,0 +1,32 @@
+use clap::Parser;
+
+use crate::{
+    currencies::{CurrencyPair, SUPPORTED_CURRENCY_PAIRS},
+    Result,
+};
+
+pub fn parse_arguments() -> Result<(CurrencyPair, u16)> {
+    let CliArgs {
+        currency_pair,
+        port,
+    } = CliArgs::parse();
+
+    let currency_pair = currency_pair.parse()?;
+
+    Ok((currency_pair, port))
+}
+
+/// gRPC server that streams book order information for a currency pair.
+#[derive(Parser, Debug)]
+struct CliArgs {
+    /// Currency pair for which the information will be fetched and streamed.
+    #[clap(
+        default_value = "ETHBTC",
+        possible_values = SUPPORTED_CURRENCY_PAIRS
+    )]
+    pub currency_pair: String,
+
+    /// Port where the server will be served.
+    #[clap(default_value = "50051")]
+    pub port: u16,
+}
